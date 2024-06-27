ecfb64295fbcdd4cb8800561d8bd6067ac0c6dff
KaporalClems <clement.tossam.dsr@gmail.com>
2024-06-27 11:21:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const mountains = [
         { name: 'Piton des Neiges', latitude: -21.099, longitude: 55.479, altitude: 3070 },
         { name: 'Grand Bénare', latitude: -21.115049140305235, longitude: 55.423090308826986, altitude: 2898 },
-        // Add more mountains of Cilaos here if needed
+        { name: 'Col du Taïbit', latitude: -21.132, longitude: 55.460, altitude: 2080 },
+        { name: 'Bonnet de Prêtre', latitude: -21.119, longitude: 55.445, altitude: 2411 }
     ];
 
     mountains.forEach(mountain => {
